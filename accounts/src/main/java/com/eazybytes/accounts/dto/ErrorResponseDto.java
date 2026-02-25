f3607df145ff2b39cb7a99963db1ad2660442d60
JFRG28 <fgrodriguez.28@gmail.com>
2026-02-24 18:03:51 -0600

    created: constants, mappers and Interfaces which will be implemented

--- accounts/src/main/java/com/eazybytes/accounts/dto/ErrorResponseDto.java
+++ accounts/src/main/java/com/eazybytes/accounts/dto/ErrorResponseDto.java
@@ -3,7 +3,6 @@ package com.eazybytes.accounts.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.http.HttpStatus;
-
 import java.time.LocalDateTime;
 
 @Data
@@ -15,5 +14,5 @@ public class ErrorResponseDto {
     private HttpStatus errorCode;
     private String errorMsg;
     private LocalDateTime errorTime;
-    
+
 }
---
+++ accounts/src/main/java/com/eazybytes/accounts/service/impl/AccountServiceImpl.java
@@ -0,0 +1,25 @@
+package com.eazybytes.accounts.service.impl;
+
+import com.eazybytes.accounts.dto.CustomerDto;
+import com.eazybytes.accounts.repository.AccountRepository;
+import com.eazybytes.accounts.repository.CustomerRepository;
+import com.eazybytes.accounts.service.IAccountService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+
+public class AccountServiceImpl implements IAccountService {
+
+    private AccountRepository accountRepository;
+    private CustomerRepository customerRepository;
+
+    /**
+     * @param customerDto - CustomerDto Object
+     */
+    @Override
+    public void createAccount(CustomerDto customerDto) {
+
+    }
+}

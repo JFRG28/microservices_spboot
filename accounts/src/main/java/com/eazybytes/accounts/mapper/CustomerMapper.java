11028662fa781841cb00d221c236676baf0833d7
JFRG28 <fgrodriguez.28@gmail.com>
2026-02-24 18:46:34 -0600

    added: exceptions, handler exception, modified repos and controller

--- accounts/src/main/java/com/eazybytes/accounts/mapper/CustomerMapper.java
+++ accounts/src/main/java/com/eazybytes/accounts/mapper/CustomerMapper.java
@@ -18,5 +18,5 @@ public class CustomerMapper {
         customer.setMobileNumber(customerDto.getMobileNumber());
         return customer;
     }
-    
+
 }
--- accounts/src/main/java/com/eazybytes/accounts/repository/AccountRepository.java
+++ accounts/src/main/java/com/eazybytes/accounts/repository/AccountRepository.java
@@ -1,9 +1,10 @@
 package com.eazybytes.accounts.repository;
 
+import com.eazybytes.accounts.entity.Account;
 import com.eazybytes.accounts.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AccountRepository extends JpaRepository<Customer,Long> {
+public interface AccountRepository extends JpaRepository<Account,Long> {
 }

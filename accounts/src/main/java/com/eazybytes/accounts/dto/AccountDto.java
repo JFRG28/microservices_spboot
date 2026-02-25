a66be26eaa7d6d71df5af99c4f8efbe2081892ce
JFRG28 <fgrodriguez.28@gmail.com>
2026-02-25 17:13:01 -0600

    added input validations

--- accounts/src/main/java/com/eazybytes/accounts/dto/AccountDto.java
+++ accounts/src/main/java/com/eazybytes/accounts/dto/AccountDto.java
@@ -1,13 +1,21 @@
 package com.eazybytes.accounts.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 
 public class AccountDto {
 
+    @NotEmpty(message = "Account number can't be a null or empty.")
+    @Pattern(regexp = "(^$[0-9]{10})",message = "Account number must be 10 digits. Numbers only.")
     private Long accountNumber;
+
+    @NotEmpty(message = "Account type number can't be a null or empty.")
     private String accountType;
+
+    @NotEmpty(message = "Branch address can't be a null or empty.")
     private String branchAddress;
 
 }

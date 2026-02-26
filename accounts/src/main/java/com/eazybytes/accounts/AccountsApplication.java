d8dbbaa97fae05ebb1e92c3fa8b9dc0004a87019
JFRG28 <fgrodriguez.28@gmail.com>
2026-02-25 18:58:56 -0600

    enhanced documentation using OpenAPIDodumentation anottation in main class

--- accounts/src/main/java/com/eazybytes/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/eazybytes/accounts/AccountsApplication.java
@@ -1,11 +1,36 @@
 package com.eazybytes.accounts;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Accounts microservice REST API Documentation",
+				description = "PacoBank accounts microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Paco RG",
+						email = "fgrodriguez.28@gmail.com",
+						url = "www.linkedin.com/in/josé-francisco-rodríguez-garcía-a82149375"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://github.com/JFRG28/microservices_spboot"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "PacoBank accounts microservice REST API Documentation",
+				url = "http://localhost:8080/swagger-ui/index.html"
+		)
+)
 public class AccountsApplication {
 
 	public static void main(String[] args) {

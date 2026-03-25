42f2f530646856a49ca8ce687f4047c645118d44
JFRG28 <fgrodriguez.28@gmail.com>
2026-03-25 13:33:50 -0600

    implemented rate limiter pattern in accounts microservice without using Redis

--- accounts/src/main/java/com/eazybytes/accounts/controller/AccountController.java
+++ accounts/src/main/java/com/eazybytes/accounts/controller/AccountController.java
@@ -6,6 +6,7 @@ import com.eazybytes.accounts.dto.CustomerDto;
 import com.eazybytes.accounts.dto.ErrorResponseDto;
 import com.eazybytes.accounts.dto.ResponseDto;
 import com.eazybytes.accounts.service.IAccountService;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -16,6 +17,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -35,6 +37,7 @@ public class AccountController {
 
     private final IAccountService iAccountService;
     private final AccountsContactInfoDto accountsContactInfoDto;
+    private final Environment environment;
 
     @Operation(
             summary = "Create Account REST API",
@@ -193,6 +196,20 @@ public class AccountController {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(dummyContactInfo);
+    }
+
+    @RateLimiter(name = "getJavaVersion",fallbackMethod = "getJavaVersionFallback")
+    @GetMapping("/java-version")
+    public ResponseEntity<String> getJavaVersion() {
+        return  ResponseEntity
+                .status(HttpStatus.OK)
+                .body(environment.getProperty("JAVA_HOME"));
+    }
+
+    public ResponseEntity<String> getJavaVersionFallback(Throwable throwable) {
+        return  ResponseEntity
+                .status(HttpStatus.OK)
+                .body("Java version is currently unavailable. Please try again later.");
     }
 
 }
